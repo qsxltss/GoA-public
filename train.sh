25bd8e5d88078428f90e20cd6b16724c15052798
qsxltss <2401112059@stu.pku.edu.cn>
2025-04-16 16:21:20 +0800

    4.16：修改train.sh

--- train.sh
+++ train.sh
@@ -9,7 +9,6 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         --gpus) GPUS="$2"; shift ;;
         --dataset) DATASET="$2"; shift ;;
-        --model) MODEL="$2"; shift ;;
         --output_dir) OUTPUT_DIR="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
